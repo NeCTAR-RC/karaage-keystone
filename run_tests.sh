c67a35f2029837bc1cbb732fe0761ba213b5b518
Russell Sim <russell.sim@gmail.com>
2014-03-17 16:31:10 +1100

    Added flake8 check
    Change-Id: I38cadb4fc9329811a7c5fbbadd794cd54caf5f5c

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,14 @@ DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 RETURN=0
 cd $DIR
 
-flake8 .
+echo -e "\nFlake8"
+echo "#########################"
+./flake8-diff.py --changed --verbose
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
 if [ ! $? -eq 0 ]
 then
     RETURN=1
