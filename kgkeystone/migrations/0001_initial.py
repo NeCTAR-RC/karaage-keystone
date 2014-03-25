96e3a6945632268724804b703a612b408bbb4b61
Russell Sim <russell.sim@gmail.com>
2014-03-26 10:51:18 +1100

    Fixed bug from migrating south code
    Regenerated south migration.
    
    Change-Id: I8fde9cdd83cc0084ae5d705a9b96650c3c256fb5

--- kgkeystone/migrations/0001_initial.py
+++ kgkeystone/migrations/0001_initial.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'ProjectRenamed'
-        db.create_table(u'kguser_projectrenamed', (
+        db.create_table(u'kgkeystone_projectrenamed', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['projects.Project'])),
+            ('project', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'has_been_renamed', to=orm['projects.Project'])),
             ('renamed', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal(u'kguser', ['ProjectRenamed'])
+        db.send_create_signal(u'kgkeystone', ['ProjectRenamed'])
 
 
     def backwards(self, orm):
         # Deleting model 'ProjectRenamed'
-        db.delete_table(u'kguser_projectrenamed')
+        db.delete_table(u'kgkeystone_projectrenamed')
 
 
     models = {
         u'institutes.institute': {
             'Meta': {'ordering': "['name']", 'object_name': 'Institute', 'db_table': "'institute'"},
-            'delegates': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'delegate'", 'to': u"orm['people.Person']", 'through': u"orm['institutes.InstituteDelegate']", 'blank': 'True', 'symmetrical': 'False', 'null': 'True'}),
+            'delegates': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'delegate_for'", 'to': u"orm['people.Person']", 'through': u"orm['institutes.InstituteDelegate']", 'blank': 'True', 'symmetrical': 'False', 'null': 'True'}),
             'group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Group']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
@@ -39,17 +37,17 @@ class Migration(SchemaMigration):
             'person': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Person']"}),
             'send_email': ('django.db.models.fields.BooleanField', [], {})
         },
-        u'kguser.projectrenamed': {
+        u'kgkeystone.projectrenamed': {
             'Meta': {'object_name': 'ProjectRenamed'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'has_been_renamed'", 'to': u"orm['projects.Project']"}),
             'renamed': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         },
         u'people.group': {
-            'Meta': {'ordering': "['name']", 'unique_together': "(('name', 'foreign_id'),)", 'object_name': 'Group'},
+            'Meta': {'ordering': "['name']", 'object_name': 'Group'},
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'extra_data': ('jsonfield.fields.JSONField', [], {'default': '{}'}),
-            'foreign_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'foreign_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'unique': 'True', 'null': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'members': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'groups'", 'symmetrical': 'False', 'to': u"orm['people.Person']"}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
@@ -106,11 +104,11 @@ class Migration(SchemaMigration):
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_usage': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'leaders': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'leaders'", 'symmetrical': 'False', 'to': u"orm['people.Person']"}),
+            'leaders': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'leads'", 'symmetrical': 'False', 'to': u"orm['people.Person']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'pid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'start_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 3, 6, 0, 0)'})
+            'start_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 3, 27, 0, 0)'})
         }
     }
 
-    complete_apps = ['kguser']
+    complete_apps = ['kgkeystone']
